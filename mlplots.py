1138a6c284bcf083c82af68d767823d222fe16bc
Harry Tunstall <H.Tunstall@warwick.ac.uk>
2019-06-17 15:42:04 +0100

    refactor, added sharey funcitonality for residuals

--- mlplots.py
+++ mlplots.py
@@ -7,29 +7,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ase.io 
 
-def format_plot(ax, title, fontsize=16, factor=0.004):
+def get_max_lims(ax_list, factor=0.04):
+    """
+    Get the maximum lims for the graphs
+    
+    Where:
+        ax_list: Is a list of the plot/residual axis pairs
+    """
+    maxs0 = []
+    mins1 = []
+    maxs1 = []
+    for ax in ax_list:
+        xlim    = ax[0].get_xlim()
+        ylim    = ax[0].get_ylim()
+        maxs0.append(max([xlim[1], ylim[1]]))
+        
+        ylim    = ax[1].get_ylim()
+        mins1.append(ylim[0])
+        maxs1.append(ylim[1])
+        
+    lim0 = [-factor, max(maxs0) + factor]
+    lim1 = [min(mins1) - factor, max(maxs1) + factor]
+    return lim0, lim1
+#---------------------------------------------
+
+def format_plot(ax, title, lims, fontsize=16, match_residuals=False):
     """
     Format the axes object to some specific standards.
+    
     Where:
-        ax:       A subplot list of matplotlib Axes
-        title:    The title of the Graph
-        fontsize: The font size for all elements (note titles are +2 larger)
-        factor:   The amount to pad the x and y axes
+        ax:              A subplot list of matplotlib Axes
+        title:           The title of the Graph
+        lims:            A tuple of the maximum limits for the top/bottom graph
+        fontsize:        The font size for all elements (note titles
+                            are +2 larger)
+        match_residuals: True makes the residual plots share a y axis
     """
+    lim, residual_lim = lims
+    
     #-----------------------------------------
     # Format the Graph
     #-----------------------------------------
-    # Fiddle with limits | Get the x and y limits
-    xlim      = ax[0].get_xlim()
-    ylim      = ax[0].get_ylim()
-    lim       = (ylim[0] + factor, ylim[1] - factor)
-    lim_width = lim[1] - lim[0]
-
     # Draw the lines
     ax[0].plot(lim, lim, color="k") # label="$x = y$"
     ax[1].hlines(0, lim[0], lim[1])
 
-    # Make the above plot square
+    # Make the top plot square
     ax[0].set_xlim(lim)
     ax[0].set_ylim(lim)
 
@@ -37,8 +60,10 @@ def format_plot(ax, title, fontsize=16, factor=0.004):
     ax[0].grid()
     ax[1].grid()
 
-    # Make sure the below plot uses the same limits to lign up the data points
+    # Make sure the bottom plot uses the same limits to lign up the data points
     ax[1].set_xlim(lim)
+    if match_residuals:
+        ax[1].set_ylim(residual_lim)
 
     # Plot the legends
     ax[0].legend(loc=9, ncol=2, fontsize=fontsize-2, frameon=True, fancybox=True, shadow=True)
@@ -58,13 +83,14 @@ def format_plot(ax, title, fontsize=16, factor=0.004):
     ax2_height = ulim - llim
 
     # Draw the figure letter
-    ax[0].text(lim[0] + lim_width / 100, lim[1] - abs(lim[0] - lim[1])  / 20, "(a)", fontsize=26)
-    ax[1].text(lim[0] + lim_width / 100, ulim   - ax2_height / 11, "(b)", fontsize=26)
+    ax[0].text(lim[0] + (lim[1] - lim[0]) / 100, lim[1] - abs(lim[0] - lim[1])  / 20, "(a)", fontsize=26)
+    ax[1].text(lim[0] + (lim[1] - lim[0]) / 100, ulim   - ax2_height / 11, "(b)", fontsize=26)
 #---------------------------------------------
 
 def files(regex):
     """
     Returns a list of lists containing filenames  (index 0) and it's associated epoch (index 1).
+    
     Where:
         regex: Is a compiled regular expression object for locating the specific epoch(s) chosen
     """
@@ -81,6 +107,7 @@ def files(regex):
 def get_min_values(files_to_plot):
     """
     Get the minimum value for each epoch and return a list of them.
+    
     Where:
         files_to_plot: Is the output of the files definition above
     """
@@ -106,6 +133,7 @@ def get_min_values(files_to_plot):
 def plot_NN_performance(best_epochs, ax, min_value, _type):
     """
     Plot the neural network data.
+    
     Where:
         best_epochs: Is either a number or a list of numbers corresponding to the epochs to plot
         ax:          Is a numpy aray of array of axes objects
@@ -142,6 +170,7 @@ def plot_NN_performance(best_epochs, ax, min_value, _type):
 def plot_gap(filename, label, ax, colour):
     """
     Plot the GAP data.
+    
     Where:
         filename: Is the path to the saved numpy array containing the x (col 0) and y (col 1) data.
         label:    Is the legend label string
@@ -164,65 +193,3 @@ def plot_gap(filename, label, ax, colour):
     # Residuals
     residuals = y - x
     ax[1].scatter(x, residuals, marker="o", color=colour, label="Residuals ({})".format(label))
-#=============================================
-
-# Make sure we're in the right dir
-start_dir = "/storage/chem/msufgx/postgrad/software/SiC-framework/testing-dir/RuNNer/generation-001"
-os.chdir(start_dir)
-
-fig, ax = plt.subplots(2, 2, gridspec_kw={'height_ratios':[2, 1]}, figsize=(24,15), sharex="col")
-ax_NN  = [ax[0,0], ax[1,0]]
-ax_GAP = [ax[0,1], ax[1,1]]
-
-fig.subplots_adjust(hspace=0)
-
-energy_data_train_re = re.compile("trainpoints\.(\d+)\.out")
-energy_data_test_re  = re.compile("testpoints\.(\d+)\.out")
-
-best_epochs = [20]
-
-train_files_to_plot = files(energy_data_train_re)
-test_files_to_plot  = files(energy_data_test_re)
-
-# Get the minimum values in the epochs we selected.
-list_min_values = np.array([get_min_values(train_files_to_plot), get_min_values(test_files_to_plot)])
-width = len(list_min_values[0,:])
-min_values = [None] * width
-for i in range(width):
-    min_values[i] = min(list_min_values[:,i])
-    
-#=============================================
-# Plot the NN Data
-#=============================================
-best_epochs = [20]
-
-train_files_to_plot = files(energy_data_train_re)
-test_files_to_plot  = files(energy_data_test_re)
-
-for filename, epoch in train_files_to_plot:
-    # Training
-    plot_NN_performance(best_epochs, ax_NN, min_value, "Train")
-
-for filename, epoch in test_files_to_plot:
-    # Testing
-    plot_NN_performance(best_epochs, ax_NN, min_value, "Test")
-    
-#=============================================
-# Plot GAP Results
-#=============================================
-# Make sure we're in the right dir
-start_dir = "/storage/chem/msufgx/postgrad/software/SiC-framework/testing-dir/GAP/tinis/gen-001/nmax7_lmax8_500nsparse"
-os.chdir(start_dir)
-
-plot_gap("gen001_training_nmax7_lmax8_nsparse500.txt", "Train", ax_GAP, "C2")
-plot_gap("gen001_testing_nmax7_lmax8_nsparse500.txt",  "Test",  ax_GAP, "C3")
-
-#=============================================
-# Format the Graph
-#=============================================
-format_plot(ax_NN, "Energy of NN Potential Vs. DFT Data", fontsize=20)
-format_plot(ax_GAP, "Energy of GAP Potential Vs. DFT Data", fontsize=20)
-
-fig.savefig("GAP_NN_data.pdf", bbox_inches='tight')
-fig.savefig("GAP_NN_data.png", dpi=300, transparent=False, bbox_inches='tight') # Publication: dpi=600, transparent=True
-
