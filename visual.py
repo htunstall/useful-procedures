0304589b5e44096f656bdb06ffed1b509bd0bddf
Harry Tunstall <h.tunstall@warwick.ac.uk>
2020-05-11 10:53:16 +0100

    Added docstrings

--- visual.py
+++ visual.py
@@ -1,13 +1,25 @@
-import inspect, re
+import inspect
+import re
+
 #================================================
 # Externally called procedures
 #================================================
 def show(p):
-  for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
-    m = re.search(r'\bshow\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
-    if m:
-      print("{} = {}".format(m.group(1), p))
+	"""Print a variable name and it's value.
+	
+	Arguments:
+		p      -- the variable to print
+	"""
+    for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
+		m = re.search(r'\bshow\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
+		if m:
+			print("{} = {}".format(m.group(1), p))
 
 def h_line(_i=120, _symbol="-"):
-    for i in range(_i): print(_symbol, end="")
-    print("")
+	"""Draw a horizontal line.
+	
+	Optional Arguments:
+		_i      -- length of the line
+		_symbol -- the symbol to repeat _i times
+	"""
+    print(_symbol * _i + "\n")
