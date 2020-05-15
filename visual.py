a1d62305b5e234e9a2f1ec9325b9e38ccab45e6a
Harry Tunstall <H.Tunstall@warwick.ac.uk>
2020-05-15 10:00:47 +0100

    fixed indenting

--- visual.py
+++ visual.py
@@ -5,21 +5,21 @@ import re
 # Externally called procedures
 #================================================
 def show(p):
-	"""Print a variable name and it's value.
-	
-	Arguments:
-		p      -- the variable to print
-	"""
+    """Print a variable name and it's value.
+
+    Arguments:
+        p      -- the variable to print
+    """
     for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
-		m = re.search(r'\bshow\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
-		if m:
-			print("{} = {}".format(m.group(1), p))
+        m = re.search(r'\bshow\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
+        if m:
+            print("{} = {}".format(m.group(1), p))
 
 def h_line(_i=120, _symbol="-"):
-	"""Draw a horizontal line.
-	
-	Optional Arguments:
-		_i      -- length of the line
-		_symbol -- the symbol to repeat _i times
-	"""
+    """Draw a horizontal line.
+
+    Optional Arguments:
+        _i      -- length of the line
+        _symbol -- the symbol to repeat _i times
+    """
     print(_symbol * _i)
